8681be640c78b5d7bf66c4177684bab7b25e05a1
yoshinorin <yoshinorin.net@outlook.com>
2024-04-06 00:41:08 +0900

    refactor: move `WebSocket` code from `notifier` to `ws`

--- src/notifier.ts
+++ src/notifier.ts
@@ -1,34 +1,13 @@
 import type Hexo from 'hexo';
-import WebSocket from 'ws';
+
 import { logger } from 'hexo-log';
 import { Config, getOrDefault } from './config';
+import { makeWss } from './ws';
 
 const log = logger({
   debug: false,
   silent: false
 });
-
-// TODO: move other file or...
-export const makeWss = (config: Config) => {
-  const wss = new WebSocket.Server({
-    port: config.server.port
-  });
-
-  // TODO: error handling. etc...
-
-  // TODO: consider log format.
-  log.info(`[Browser Reloader]: settings - ${JSON.stringify(config, null, 2)}`);
-
-  wss.on('error', (err) => {
-    log.error(`[Browser Reloader]: ${err}`);
-  });
-
-  wss.on('connection', () => {
-    log.info(`[Browser Reloader]: Connection established.`)
-  });
-
-  return wss;
-};
 
 export function notifier(this: Hexo) {
   const self = this;
---
+++ src/ws.ts
@@ -0,0 +1,27 @@
+import { Config } from './config';
+import WebSocket from 'ws';
+import { logger } from 'hexo-log';
+
+const log = logger({
+  debug: false,
+  silent: false
+});
+
+export const makeWss = (config: Config) => {
+  const wss = new WebSocket.Server({
+    port: config.server.port
+  });
+
+  // TODO: consider log format.
+  log.info(`[Browser Reloader]: settings - ${JSON.stringify(config, null, 2)}`);
+
+  wss.on('error', err => {
+    log.error(`[Browser Reloader]: ${err}`);
+  });
+
+  wss.on('connection', () => {
+    log.info('[Browser Reloader]: Connection established.');
+  });
+
+  return wss;
+};
