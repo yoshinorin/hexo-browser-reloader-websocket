48e23244781a8cd841934d246a7f02bd7ae3e8d8
yoshinorin <yoshinorin.net@outlook.com>
2024-04-09 00:09:29 +0900

    ci: setup GitHub Actions and upload coverage report to GitHub Pages

---
+++ vitest.config.js
@@ -0,0 +1,16 @@
+import { defineConfig } from 'vitest/config'
+
+export default defineConfig({
+  test: {
+    // globals: true
+    include: [
+      './test/**/*.{test,spec}.?(c|m)[jt]s?(x)'
+    ],
+    coverage: {
+      reporter: ['text', 'html'],
+      reportsDirectory: './coverage',
+      exclude: [
+      ],
+    }
+  },
+})
