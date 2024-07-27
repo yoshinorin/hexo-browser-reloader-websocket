ec400714fcea99bb889468b75b80b5d618e22e92
yoshinorin <yoshinorin.net@outlook.com>
2024-07-28 00:53:23 +0900

    fix: return `false` if `hexo cmd` is `undefined`

--- src/utils.ts
+++ src/utils.ts
@@ -3,6 +3,9 @@ import { logger } from 'hexo-log';
 import { Config } from './config';
 
 export const isServerMode = (hexo: Hexo): boolean => {
+  if (hexo.env === undefined || hexo.env.cmd === undefined) {
+    return false;
+  }
   return hexo.env.cmd.toLowerCase() === 'server' || hexo.env.cmd.toLowerCase() === 's';
 };
 
